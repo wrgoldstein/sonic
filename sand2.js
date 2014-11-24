eab93cf68737d4a90833c92ed9ce73916a5d7880
wrgoldstein <williamrgoldstein@gmail.com>
2014-11-23 20:54:11 -0500

    a

---
+++ sand2.js
@@ -0,0 +1,107 @@
+var offset_height = document.getElementById("sonic-bg").offsetHeight,
+    offset_width = document.getElementById("sonic-bg").offsetWidth,
+    stepsize = 5,
+    steps = offset_width / stepsize;
+
+(function() {
+    p = document.getElementById("little_person")
+    p.setAttribute("x", offset_width / 2)
+    p.setAttribute("y", offset_height - 50);
+})();
+
+
+var margin = {
+        top: 200,
+        right: 50,
+        bottom: 200,
+        left: 50
+    },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.bottom - margin.top;
+
+var x = d3.scale.linear()
+    .domain([0, steps])
+    .range([0, document.getElementById("sliderbox").offsetWidth])
+    .clamp(true);
+
+var brush = d3.svg.brush()
+    .x(x)
+    .extent([0, 0])
+    .on("brush", brushed);
+
+svg = d3.select("#sliderbox");
+
+var rocketslider = svg.append("g")
+    .attr("class", "rocketslider")
+    .call(brush);
+
+var handle = rocketslider.append("ellipse")
+    .attr("class", "handle")
+    .attr("id", "handle")
+    .attr("rx", 10).attr("ry", 150);
+
+// var handle = d3.select("#rocket");
+
+sound = {
+    init: function() {
+        this.radii = new Array();
+        this.pressures = new Array();
+        for (i = 0; i < offset_width / stepsize; i++) {
+            this.radii.push(i * stepsize);
+            this.pressures.push()
+        };
+        // this.radii.reverse();
+        // this.pressures.reverse();
+    },
+
+    update: function(state) {
+        console.log(state * stepsize);
+        rocket.setAttribute("y", -state * stepsize)
+        state_radii = this.radii.slice(0, state);
+        circles = d3.select(".soundwaves")
+            .selectAll("circle").data(state_radii.reverse());
+        circles.exit().remove();
+        circles.enter()
+            .append("circle")
+            .attr("cx", function(d, i) {
+                return state * stepsize
+            })
+            .attr("cy", 45)
+            .style("fill-opacity", 0)
+            .style("stroke", "white")
+            .style("stroke-width", 3);
+
+        circles.attr("r", function(d) {
+            if (d == "NaN") {
+                return null
+            }
+            return d;
+        }).attr("stroke-opacity", function(d) {
+            return .5 - d / offset_width
+        })
+
+    }
+}
+
+sound.init();
+
+
+function brushed() {
+    var value = brush.extent()[0];
+
+    if (d3.event.sourceEvent) { // not a programmatic event
+        value = x.invert(d3.mouse(this)[0]);
+        brush.extent([value, value]);
+    }
+
+    handle.attr("cx", x(value));
+    sound.update(value);
+}
+
+rocketslider
+    .call(brush.event)
+    .transition() // gratuitous intro!
+    .duration(1500)
+    .ease("linear")
+    .call(brush.extent([150, 150]))
+    .call(brush.event);
